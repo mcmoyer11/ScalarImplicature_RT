bb79d4576f334726d3de4aa70ee46ae7e79af958
Morgan Moyer <mcmoyer11@gmail.com>
2020-04-15 08:54:36 -0400

    add DashedSentence

--- data_includes/bn_main.js
+++ data_includes/bn_main.js
@@ -2,7 +2,15 @@ PennController.ResetPrefix(null); // Initiates PennController
 
 // BOTT & NOVECK TRIALS
 
-Sequence("intro","consent", "demo","instructions", "trainT", "trainF","train_thinkT","train_thinkF", "end_train", rshuffle("trial"), "send", "final")
+Sequence("intro","consent", "demo","instructions", "Train", "end_train", rshuffle("test"), "send", "end")
+// Sequence("demo","instructions", "Train", "send")
+
+
+// This only works when I don't embed a DashedSentence in a newController()
+var defaults = [
+    "DashedSentence", {mode: "speeded acceptability", wordTime: 250}
+    ];
+
 
 newTrial("intro",
     defaultText
@@ -22,6 +30,7 @@ newTrial("intro",
         .print()
         .wait()
 )
+.setOption("hideProgressBar", true); // Do not show the progress bar on first screen
 
 newTrial("consent",
     newHtml("consent.html")
@@ -52,6 +61,7 @@ newTrial("consent",
         .set( getTextInput("ID") )
 )
 .log( "ID" , getVar("ID") )
+.setOption("hideProgressBar", true); // Do not show the progress bar on first screen
 
 newTrial("demo",
     defaultText
@@ -92,21 +102,7 @@ newTrial("demo",
 )
 .log( "NativeLang" , getVar("NativeLang") )
 .log( "OtherLangs" , getVar("OtherLangs") )
-
-
-// What is in Header happens at the beginning of every single trial
-Header(
-    // We will use this global Var element later to store the participant's name
-    newVar("ID")
-        .global()
-    // ,
-    // // Delay of 250ms before every trial
-    // newTimer(250)
-    //     .start()
-    //     .wait()
-)
-.log( "ID" , getVar("ID") )
-// This log command adds a column reporting the participant's name to every line saved to the results
+.setOption("hideProgressBar", true); // Do not show the progress bar on first screen
 
 
 newTrial("instructions",
@@ -116,148 +112,82 @@ newTrial("instructions",
     ,
     newText("<p>In this experiment, we will ask you to decide whether you agree or disagree with a statement.</p>")
     ,
-    newButton("Let's practice!")
+    newText("<p>The statement will be revealed automatically, one word at a time.</p>")
+    ,
+    newText("<p>As each new word appears, the previous word will disappear.</p>")
+    ,
+    newText("<p>If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
+    ,
+    newText("<p>Let's <strong>practice</strong> a few times before proceeding to the experiment.<p>")
+    ,
+    newButton("Click here when you're ready to Continue.")
         .center()
         .print()
         .wait()
 )
 
-
-newTrial( "trainT",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("<p><strong>PRACTICE.</strong></p>")
+Template( "train_table.csv", row =>
+    [
+    "Train",
+    "DashedSentence", {s: row.Sentence},
+    "PennController", newTrial("question",    
+        newText("<p> Press <strong>F</strong> to <strong>Agree</strong> or <strong>J</strong> to <strong>Disagree</strong><p>")
             .center()
             .print()
         ,
-        newText( "<p>Houses can be made of wood.</p>" )
+        newText(row.Feedback)
             .center()
             .print()
-        ,
-        newText("<p> If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
-            .center()
-            .print()
-        ,
-        newText("<p>Since this statement is <strong>correct</strong>, you should press <strong>F</strong> on the keyboard to agree.<p>")
-            .center()
-            .print()
+            .log( "Feedback" , row.Feedback )
         ,
         newSelector()
             .add( newText("Agree"), newText("Disagree"))
-            .center()
             .keys("F", "J")
-            .log()
+            .log("first")
             .wait()
-        ,
-        newTimer(500)
-            .start()
-            .wait()
+        )
+        .log( "ID" , getVar("ID") )
+        .log( "Type" , row.Type )
+        .log( "Sentence" , row.Sentence )
+        .log( "Feedback" , row.Feedback )
+    ]
 )
 
-newTrial( "trainF",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("<p><strong>PRACTICE.</strong></p>")
-            .center()
-            .print()
-        ,
-        newText( "<p>Airplanes fly under water.</p>" )
-            .center()
-            .print()
-        ,
-        newText("<p> If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
-            .center()
-            .print()
-        ,
-        newText("<p>Since this statement is <strong>incorrect</strong>, you should press <strong>J</strong> on the keyboard to disagree.<p>")
-            .center()
-            .print()
-        ,
-        newSelector()
-            .add( newText("Agree"), newText("Disagree"))
-            .center()
-            .keys("F", "J")
-            .log()
-            .wait()
-        ,
-        newTimer(500)
-            .start()
-            .wait()
-)
+// Template( "train_table.csv", row =>
+//     newTrial("Train",
+//         newText("<p><strong>PRACTICE</strong></p>")
+//             .center()
+//             .print()
+//         ,
+//         newController("DashedSentence", {s: row.Sentence})
+//             .center()
+//             .print()
+//             .log()
+//             .wait()
+//         ,
+//         newText("<p> Press <strong>F</strong> to <strong>Agree</strong> or <strong>J</strong> to <strong>Disagree</strong><p>")
+//             .center()
+//             .print()
+//         ,
+//         newText(row.Feedback)
+//             .center()
+//             .print()
+//         ,
+//         newSelector()
+//             .add( newText("Agree"), newText("Disagree"))
+//             .keys("F", "J")
+//             .log("first")
+//             .wait()
+//     )
+//     .log( "ID" , getVar("ID") )
+//     .log( "Type" , row.Type )
+//     .log( "Sentence" , row.Sentence )
+//     .log( "Feedback" , row.Feedback )
+// )
 
-newTrial( "train_thinkT",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("<p><strong>PRACTICE.</strong></p>")
-            .center()
-            .print()
-        ,
-        newText( "<p>Historians think that George Washington was a US president.</p>" )
-            .center()
-            .print()
-        ,
-        newText("<p> If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
-            .center()
-            .print()
-        ,
-        newText("<p>Since this statement is <strong>correct</strong>, you should press <strong>F</strong> on the keyboard to agree.<p>")
-            .center()
-            .print()
-        ,
-        newSelector()
-            .add( newText("Agree"), newText("Disagree"))
-            .center()
-            .keys("F", "J")
-            .log()
-            .wait()
-        ,
-        newTimer(500)
-            .start()
-            .wait()
-)
-
-newTrial( "train_thinkF",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("<p><strong>PRACTICE.</strong></p>")
-            .center()
-            .print()
-        ,
-        newText( "<p>Geographers think that Spain is in Africa.</p>" )
-            .center()
-            .print()
-        ,
-        newText("<p> If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
-            .center()
-            .print()
-        ,
-        newText("<p>Since this statement is <strong>incorrect</strong>, you should press <strong>J</strong> on the keyboard to disagree.<p>")
-            .center()
-            .print()
-        ,
-        newSelector()
-            .add( newText("Agree"), newText("Disagree"))
-            .center()
-            .keys("F", "J")
-            .log()
-            .wait()
-        ,
-        newTimer(500)
-            .start()
-            .wait()
-)
 
 
 newTrial("end_train",
-    
     defaultText
         .center()
         .print()
@@ -272,45 +202,71 @@ newTrial("end_train",
         .wait()
 )
 
+// This works to get speeded acceptability, but the results look weird
+// Template( "bn_table.csv", row =>
+//         ["test",
+//         "DashedSentence", {s: `${row.Matrix} ${row.Quantifier} ${row[row.Subject.replace("Category",row.WhichCategory+'Category')]} are ${row[row.Predicate.replace("Category",row.WhichCategory+'Category')]}`},
+//         "PennController", newTrial("question",    
+//             newText("<p> Press <strong>F</strong> to <strong>Agree</strong> or <strong>J</strong> to <strong>Disagree</strong><p>")
+//                 .center()
+//                 .print()
+//             ,
+//             newSelector()
+//                 .add( newText("Agree"), newText("Disagree"))
+//                 .keys("F", "J")
+//                 .log("first")
+//                 .wait()
+//         )
+//         .log( "ID" , getVar("ID") )
+//         .log( "SentNumber" , row.SentNumber )
+//         .log( "Quantifier" , row.Quantifier )
+//         .log( "Matrix" , row.Matrix )
+//         .log( "WhichCategory" , row.WhichCategory )
+//         .log( "Subject" , row.Subject )
+//         .log( "Predicate" , row.Predicate )
+//         .log( "SentType" , row.SentType )
+//         .log( "Verb" , row.Verb )
+//         .log( "Embedded" , row.Embedded )
+//         .log( "Token" , row.Token )
+//         .log( "MatchingCategory" , row.MatchingCategory )
+//         .log( "MismatchingCategory" , row.MismatchingCategory )
+//         .log( "Group"  , row.Group  )
+//     ]
+// )
 
+// With this I cannot get speeded acceptability
 Template( "bn_table.csv", row =>
-    newTrial( "trial",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("sentence", `${row.Matrix} ${row.Quantifier} ${row[row.Subject.replace("Category",row.WhichCategory+'Category')]} are ${row[row.Predicate.replace("Category",row.WhichCategory+'Category')]}` )
+        newTrial("test"
+            newController("DashedSentence", {s: `${row.Matrix} ${row.Quantifier} ${row[row.Subject.replace("Category",row.WhichCategory+'Category')]} are ${row[row.Predicate.replace("Category",row.WhichCategory+'Category')]}`})
             .center()
             .print()
-        ,
-        newText("<p> Press <strong>F</strong> to <strong>Agree</strong> or <strong>J</strong> to <strong>Disagree</strong><p>")
-            .center()
-            .print()
-        ,
-        newSelector()
-            .add( newText("Agree"), newText("Disagree"))
-            .keys("F", "J")
             .log()
             .wait()
         ,
-        newTimer(500)
-            .start()
-            .wait()
-    )
-    .log( "ID" , getVar("ID") )
-    .log( "SentNumber" , row.SentNumber )
-    .log( "Quantifier" , row.Quantifier )
-    .log( "Matrix" , row.Matrix )
-    .log( "WhichCategory" , row.WhichCategory )
-    .log( "Subject" , row.Subject )
-    .log( "Predicate" , row.Predicate )
-    .log( "SentType" , row.SentType )
-    .log( "Verb" , row.Verb )
-    .log( "Embedded" , row.Embedded )
-    .log( "Token" , row.Token )
-    .log( "MatchingCategory" , row.MatchingCategory )
-    .log( "MismatchingCategory" , row.MismatchingCategory )
-    .log( "Group"  , row.Group  )
+            newText("<p> Press <strong>F</strong> to <strong>Agree</strong> or <strong>J</strong> to <strong>Disagree</strong><p>")
+                .center()
+                .print()
+            ,
+            newSelector()
+                .add( newText("Agree"), newText("Disagree"))
+                .keys("F", "J")
+                .log("first")
+                .wait()
+        )
+        .log( "ID" , getVar("ID") )
+        .log( "SentNumber" , row.SentNumber )
+        .log( "Quantifier" , row.Quantifier )
+        .log( "Matrix" , row.Matrix )
+        .log( "WhichCategory" , row.WhichCategory )
+        .log( "Subject" , row.Subject )
+        .log( "Predicate" , row.Predicate )
+        .log( "SentType" , row.SentType )
+        .log( "Verb" , row.Verb )
+        .log( "Embedded" , row.Embedded )
+        .log( "Token" , row.Token )
+        .log( "MatchingCategory" , row.MatchingCategory )
+        .log( "MismatchingCategory" , row.MismatchingCategory )
+        .log( "Group"  , row.Group  )
 )
 
 SendResults( "send" )
